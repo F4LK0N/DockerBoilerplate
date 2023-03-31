e563f705f539897f54ff265a7c73e2784ac70740
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-03-31 02:58:25 -0300

    Adm Panel Options

--- .docker/debian/adm/panel.sh
+++ .docker/debian/adm/panel.sh
@@ -10,6 +10,11 @@ do
     echo " 1 = System Info"
     echo " 2 = Users"
     echo " 3 = Groups"
+    echo " 4 = Processes"
+    echo " 5 = Services"
+    echo " 6 = Network"
+    echo " 7 = Memory"
+    echo " 8 = Storage"
     echo " 0 = EXIT"
     echo "$ADM_DIVIDER"
 
@@ -27,6 +32,26 @@ do
 
     if [ $arg == "3" ]; then
         ./groups.sh
+    fi
+
+    if [ $arg == "4" ]; then
+        ./processes.sh
+    fi
+
+    if [ $arg == "5" ]; then
+        ./services.sh
+    fi
+
+    if [ $arg == "6" ]; then
+        ./network.sh
+    fi
+
+    if [ $arg == "7" ]; then
+        ./memory.sh
+    fi
+
+    if [ $arg == "8" ]; then
+        ./storage.sh
     fi
 
     if [ $arg != "0" ]; then
