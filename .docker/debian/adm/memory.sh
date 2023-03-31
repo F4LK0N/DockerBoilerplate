37c7236a3330fd03cb352d7e15ba3e693512ce9f
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-03-31 03:31:03 -0300

    Adm Panel Memory

---
+++ .docker/debian/adm/memory.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+full=$( cat /etc/passwd )
+short=$( cat /etc/passwd | cut -d\: -f1 )
+
+echo "$ADM_LOGO"
+echo "$ADM_DIVIDER"
+echo "### MEMORY ###"
+echo "$ADM_DIVIDER"
+echo " 1 = Commands"
+echo " 2 = Full List"
+echo " 3 = Short List"
+echo " 4 = Tree"
+echo "$ADM_DIVIDER"
+
+echo -n "Enter option: "
+read arg
+
+echo "$ADM_DIVIDER"4
+
+if [ $arg == "1" ]; then
+    echo "cat /proc/meminfo"
+fi
+
+if [ $arg == "2" ]; then
+    cat /proc/meminfo
+fi
+
+if [ $arg == "3" ]; then
+    cat /proc/meminfo | grep 'MemTotal:\|MemFree:\|MemAvailable:'
+fi
+
+if [ $arg == "4" ]; then
+    pstree
+fi
+
+echo "$ADM_DIVIDER"
