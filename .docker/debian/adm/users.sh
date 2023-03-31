78f377766dbfb3d713f70603916c6afecf47399c
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-03-31 02:19:45 -0300

    Adm Users

--- .docker/debian/adm/users.sh
+++ .docker/debian/adm/users.sh
@@ -3,6 +3,13 @@
 full=$( cat /etc/passwd )
 short=$( cat /etc/passwd | cut -d\: -f1 )
 
+echo "COMMANDS:"
+echo "- useradd"
+echo "- usermod"
+echo "- userdel"
+echo ""
+
+echo "LIST:"
 echo "$full"
 echo ""
 echo "$short"
