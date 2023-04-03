7c4a16f6eb3ead90761cb45a2ebaf2ba819d62ca
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-03 17:41:53 -0300

    Adm UsersAndGroups

--- .docker/debian/os/adm/panel.sh
+++ .docker/debian/os/adm/panel.sh
@@ -10,26 +10,24 @@ do
     echo "$ADM_DIVIDER"
     echo " 0 = [EXIT]"
     echo " 1 = System Info"
-    echo " 2 = Users"
-    echo " 3 = Groups"
-    echo " 4 = Processes"
-    echo " 5 = Services"
-    echo " 6 = Network"
-    echo " 7 = Memory"
-    echo " 8 = Storage"
+    echo " 2 = Users and Groups"
+    echo " 3 = Processes"
+    echo " 4 = Services"
+    echo " 5 = Network"
+    echo " 6 = Memory"
+    echo " 7 = Storage"
     echo "$ADM_DIVIDER"
 
     echo -n "Enter option: "
     read option
 
     if [ "$option" == "1" ]; then /adm/system.sh; fi
-    if [ "$option" == "2" ]; then /adm/users.sh; fi
-    if [ "$option" == "3" ]; then /adm/groups.sh; fi
-    if [ "$option" == "4" ]; then /adm/processes.sh; fi
-    if [ "$option" == "5" ]; then /adm/services.sh; fi
-    if [ "$option" == "6" ]; then /adm/network.sh; fi
-    if [ "$option" == "7" ]; then /adm/memory.sh; fi
-    if [ "$option" == "8" ]; then /adm/storage.sh; fi
+    if [ "$option" == "2" ]; then /adm/users_groups.sh; fi
+    if [ "$option" == "3" ]; then /adm/processes.sh; fi
+    if [ "$option" == "4" ]; then /adm/services.sh; fi
+    if [ "$option" == "5" ]; then /adm/network.sh; fi
+    if [ "$option" == "6" ]; then /adm/memory.sh; fi
+    if [ "$option" == "7" ]; then /adm/storage.sh; fi
 
 done
 
