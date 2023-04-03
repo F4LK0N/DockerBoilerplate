481b6ffe8057b96eed316945bfe816c8092f55e0
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-03 17:21:45 -0300

    Adm - System

--- .docker/debian/os/adm/system.sh
+++ .docker/debian/os/adm/system.sh
@@ -3,6 +3,8 @@
 distribution=$( cat /etc/os-release | grep PRETTY_NAME | cut -d\" -f2 )
 version=$( cat /etc/debian_version )
 
+clear
+echo "$ADM_DIVIDER"
 echo "$ADM_LOGO"
 echo "$ADM_DIVIDER"
 echo "### SYSTEM INFO ###"
@@ -10,3 +12,6 @@ echo "$ADM_DIVIDER"
 echo "Distribution: $distribution"
 echo "Version     : $version"
 echo "$ADM_DIVIDER"
+
+echo -n "Enter to continue..."
+read option
