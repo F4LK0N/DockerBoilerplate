46b38ef40d0b11d3f34598488e7459033191cb2a
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-03 17:20:48 -0300

    Adm Panel - Bash Script Improved

--- .docker/debian/os/adm/panel.sh
+++ .docker/debian/os/adm/panel.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-arg="1000"
-clear
-
-while [ $arg != "0" ]
+option="---"
+while [ "$option" != "" ] && [ "$option" != "0" ]
 do
     
+    clear
+    echo "$ADM_DIVIDER"
     echo "$ADM_LOGO"
     echo "$ADM_DIVIDER"
+    echo " 0 = [EXIT]"
     echo " 1 = System Info"
     echo " 2 = Users"
     echo " 3 = Groups"
@@ -16,49 +17,21 @@ do
     echo " 6 = Network"
     echo " 7 = Memory"
     echo " 8 = Storage"
-    echo " 0 = EXIT"
     echo "$ADM_DIVIDER"
 
     echo -n "Enter option: "
-    read arg
-    clear
+    read option
 
-    if [ $arg == "1" ]; then
-        /adm/system.sh
-    fi
-
-    if [ $arg == "2" ]; then
-        /adm/users.sh
-    fi
-
-    if [ $arg == "3" ]; then
-        /adm/groups.sh
-    fi
-
-    if [ $arg == "4" ]; then
-        /adm/processes.sh
-    fi
-
-    if [ $arg == "5" ]; then
-        /adm/services.sh
-    fi
-
-    if [ $arg == "6" ]; then
-        /adm/network.sh
-    fi
-
-    if [ $arg == "7" ]; then
-        /adm/memory.sh
-    fi
-
-    if [ $arg == "8" ]; then
-        /adm/storage.sh
-    fi
-
-    if [ $arg != "0" ]; then
-        echo -n "Enter to continue..."
-        read arg2
-        clear
-    fi
+    if [ "$option" == "1" ]; then /adm/system.sh; fi
+    if [ "$option" == "2" ]; then /adm/users.sh; fi
+    if [ "$option" == "3" ]; then /adm/groups.sh; fi
+    if [ "$option" == "4" ]; then /adm/processes.sh; fi
+    if [ "$option" == "5" ]; then /adm/services.sh; fi
+    if [ "$option" == "6" ]; then /adm/network.sh; fi
+    if [ "$option" == "7" ]; then /adm/memory.sh; fi
+    if [ "$option" == "8" ]; then /adm/storage.sh; fi
 
 done
+
+echo "EXIT"
+clear
