536af2e2a0c3fe207b9dab316e225a1220422050
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-03 23:39:54 -0300

    Adm System

--- .docker/apache/os/adm/system.sh
+++ .docker/apache/os/adm/system.sh
@@ -9,9 +9,22 @@ echo "$ADM_LOGO"
 echo "$ADM_DIVIDER"
 echo "### SYSTEM INFO ###"
 echo "$ADM_DIVIDER"
-echo "Distribution: $distribution"
-echo "Version     : $version"
+
+echo "OS Distribution : $distribution"
+echo "OS Version      : $version"
 echo "$ADM_DIVIDER"
+
+if [ -x "$(command -v httpd)" ]; then
+    httpd_version=$( httpd -v | grep version | cut -d\: -f2 | sed 's/ *$//g' )
+    httpd_version=`echo $httpd_version | sed 's/ *$//g'`
+
+    httpd_built=$( httpd -v | grep built | cut -d\: -f2 | sed 's/ *$//g' )
+    httpd_built=`echo $httpd_built | sed 's/ *$//g'`
+  
+    echo "HTTPD Version   : $httpd_version"
+    echo "HTTPD Built     : $httpd_built"
+    echo "$ADM_DIVIDER"
+fi
 
 echo -n "Enter to continue..."
 read -r -s -n 1 option
