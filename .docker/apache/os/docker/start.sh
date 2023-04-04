bdafc77fdfc2b0b7bc69c9f8e68ba1c5a4acb503
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-04 00:54:09 -0300

    Apache Root and Logs Folder

--- .docker/apache/os/docker/start.sh
+++ .docker/apache/os/docker/start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Apache - PID Lock File Release"
+echo "OS - Logs Folder"
+rm -r /logs/os/
+mkdir -p /logs/os/
+ln -s /var/log/ /logs/os/
+
+echo "Apache - Logs Folder"
+mkdir -p /logs/apache/
+
+echo "Apache - PID File"
 rm -f /usr/local/apache2/logs/httpd.pid
+rm -f /logs/apache/httpd.pid
