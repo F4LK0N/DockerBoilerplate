75465ba2b967ef0505b7886955c907e4e41ca4cf
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-03 21:52:43 -0300

    Adm Network

--- .docker/debian/os/adm/network.sh
+++ .docker/debian/os/adm/network.sh
@@ -1,27 +1,112 @@
 #!/bin/bash
 
-full=$( cat /etc/passwd )
-short=$( cat /etc/passwd | cut -d\: -f1 )
+option="---"
+while [ "$option" != "" ] && [ "$option" != "0" ]
+do
 
-echo "$ADM_LOGO"
-echo "$ADM_DIVIDER"
-echo "### NETWORK ###"
-echo "$ADM_DIVIDER"
-echo " 1 = Commands"
-echo " 2 = Realtime (iftop)"
-echo "$ADM_DIVIDER"
+    clear
+    echo "$ADM_DIVIDER"
+    echo "$ADM_LOGO"
+    echo "$ADM_DIVIDER"
+    echo "### NETWORK ###"
+    echo "$ADM_DIVIDER"
+    
+    if [ "$option" == "---" ]; then
 
-echo -n "Enter option: "
-read arg
+        echo " 0 = [BACK]"
+        echo " 1 = Commands"
+        echo " 2 = Info (Hostname, IP and MAC)"
+        echo " 3 = Interfaces List"
+        echo " 4 = Hosts File"
+        echo " 5 = Ports Binded"
+        echo " 6 = Total Traffic"
+        
+        echo " 5 = [REALTIME] Traffic (iftop)"
+        echo "$ADM_DIVIDER"
 
-echo "$ADM_DIVIDER"
+        echo -n "Enter option: "
+        read -r -s -n 1 option
 
-if [ $arg == "1" ]; then
-    echo "iftop"
-fi
+    else
 
-if [ $arg == "2" ]; then
-    iftop
-fi
+        if [ "$option" == "1" ]; then
+            echo "- hostname"
+            echo "- hostname -i"
+            echo "- cat /sys/class/net/eth*/address"
+            echo "- ls --color=auto -lAs /sys/class/net/eth*"
+            echo "- cat /etc/hosts"
+            echo "- lsof -i -n -P -R"
+            echo "- cat /proc/net/dev | grep 'Inter-\|face\|eth'"
+            
 
-echo "$ADM_DIVIDER"
+
+            
+            echo "- iftop -n -N -B -P"
+        fi
+
+        if [ "$option" == "2" ]; then
+            hostname=$( hostname )
+
+            ip_addresses=$( hostname -i )
+            ip_addresses="- ${ip_addresses//' '/$'\n- '}"
+
+            mac_addresses=$( cat /sys/class/net/eth*/address )
+            mac_addresses="- ${mac_addresses//$'\n'/$'\n- '}"
+
+            echo "Hostname      : $hostname"
+            echo ""
+            echo "IP Addresses  :"
+            echo "$ip_addresses"
+            echo ""
+            echo "MAC Addresses :"
+            echo "$mac_addresses"
+        fi
+
+        if [ "$option" == "3" ]; then
+            list=$( ls --color=auto -lAs /sys/class/net/eth* )
+            echo "$list"
+        fi
+
+        if [ "$option" == "4" ]; then
+            list=$( cat /etc/hosts )
+            echo "$list"
+        fi
+
+        if [ "$option" == "5" ]; then
+            list=$( lsof -i -n -P -R )
+            echo "$list"
+        fi
+
+        if [ "$option" == "6" ]; then
+            list=$( cat /proc/net/dev | grep 'Inter-\|face\|eth' )
+            echo "$list"
+        fi
+
+        if [ "$option" == "7" ]; then
+            list=$( cat /proc/net/dev | grep 'Inter-\|face\|eth' )
+            echo "$list"
+        fi
+
+        if [ "$option" == "90" ]; then
+            option=""
+            while [ "$option" == "" ]
+            do 
+                list=$( ps -A )
+                clear
+                echo "$list"
+                read -r -s -n 1 -t 0.25 option
+            done
+            option="DONTWAIT"
+        fi
+
+
+        if [ "$option" != "DONTWAIT" ]; then
+            echo "$ADM_DIVIDER"
+            echo -n "Enter to continue..."
+            read -r -s -n 1 option
+        fi
+        option="---"
+
+    fi
+
+done
