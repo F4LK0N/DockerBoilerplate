cde2b8653babcfc4cd1f41ba08daeee3e26a7c58
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-09 09:39:03 -0300

    Logs Clear on Container Start

--- .docker/php/os/docker/start.sh
+++ .docker/php/os/docker/start.sh
@@ -5,27 +5,31 @@ set -e
 echo "OS - Logs"
 rm -r -f /logs/os/
 mkdir -p /logs/os/
+chmod 777 /logs/os/
 ln -s /var/log/ /logs/os/
 
 
 
 echo "Apache - Logs"
-#rm -r -f /logs/apache/
+rm -r -f /logs/apache/
 mkdir -p /logs/apache/
+chmod 777 /logs/apache/
 
 
 
 echo "PHP - Logs"
 mkdir -p /logs/php/
 chmod 777 /logs/php/
-chmod 777 -R /logs/php/
 
+rm -f /logs/php/errors.log
 touch /logs/php/errors.log
 chmod 777 /logs/php/errors.log
 
+rm -f /logs/php/xdebug.log
 touch /logs/php/xdebug.log
 chmod 777 /logs/php/xdebug.log
 
+rm -r -f /logs/php/xdebug_tmp
 mkdir -p /logs/php/xdebug_tmp
 chmod 777 /logs/php/xdebug_tmp
 chmod 777 -R /logs/php/xdebug_tmp
