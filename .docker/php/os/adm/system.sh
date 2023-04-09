ba076814c3be7fc30a2b907fd30738c65f644296
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-09 06:17:51 -0300

    Adm - System

--- .docker/php/os/adm/system.sh
+++ .docker/php/os/adm/system.sh
@@ -26,5 +26,18 @@ if [ -x "$(command -v httpd)" ]; then
     echo "$ADM_DIVIDER"
 fi
 
+if [ -x "$(command -v php)" ]; then
+    php_version=$( php -v | grep "^PHP" | cut -d' ' -f2 | sed 's/ *$//g' )
+    php_version=`echo $php_version | sed 's/ *$//g'`
+
+    php_built=$( php -v | grep built | cut -d"built:" -f2 | sed 's/ *$//g' )
+    php_built=$( php -v | grep built | awk -F 'built: ' '{print $2}' | awk -F ')' '{print $1}' )
+    #php_built=`echo $php_built | sed 's/ *$//g'`
+  
+    echo "PHP Version   : $php_version"
+    echo "PHP Built     : $php_built"
+    echo "$ADM_DIVIDER"
+fi
+
 echo -n "Enter to continue..."
 read -r -s -n 1 option
