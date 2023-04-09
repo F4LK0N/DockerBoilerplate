392483a04c7b9b9628dd1b722d0201c713d801c9
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-09 08:51:48 -0300

    XDebug Config

--- .docker/php/os/docker/start.sh
+++ .docker/php/os/docker/start.sh
@@ -1,18 +1,36 @@
 #!/bin/sh
 set -e
 
+
 echo "OS - Logs"
 rm -r -f /logs/os/
 mkdir -p /logs/os/
 ln -s /var/log/ /logs/os/
 
+
+
 echo "Apache - Logs"
 #rm -r -f /logs/apache/
 mkdir -p /logs/apache/
 
+
+
 echo "PHP - Logs"
-#rm -r -f /logs/php/
 mkdir -p /logs/php/
+chmod 777 /logs/php/
+chmod 777 -R /logs/php/
+
+touch /logs/php/errors.log
+chmod 777 /logs/php/errors.log
+
+touch /logs/php/xdebug.log
+chmod 777 /logs/php/xdebug.log
+
+mkdir -p /logs/php/xdebug_tmp
+chmod 777 /logs/php/xdebug_tmp
+chmod 777 -R /logs/php/xdebug_tmp
+
+
 
 echo "Application - File System"
 mkdir -p /app/
