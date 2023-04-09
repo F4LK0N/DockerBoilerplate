a37bc969cb6a5f64d93ee454e6790f678b16c324
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-09 07:04:33 -0300

    Apache2 Conf

--- .docker/php/os/docker/start.sh
+++ .docker/php/os/docker/start.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 set -e
 
-echo "OS - Logs Folder"
+echo "OS - Logs"
 rm -r -f /logs/os/
 mkdir -p /logs/os/
 ln -s /var/log/ /logs/os/
+
+echo "Apache - Logs"
+#rm -r -f /logs/apache/
+mkdir -p /logs/apache/
+
+echo "PHP - Logs"
+#rm -r -f /logs/php/
+mkdir -p /logs/php/
+
+echo "Application - File System"
+mkdir -p /app/
+mkdir -p /app/public/
