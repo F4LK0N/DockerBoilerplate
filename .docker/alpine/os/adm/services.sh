1d58949b6b81902dfe6d4e02a36f8cd19f4afe6f
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-09 04:40:43 -0300

    Adm - Services

--- .docker/alpine/os/adm/services.sh
+++ .docker/alpine/os/adm/services.sh
@@ -16,7 +16,10 @@ do
         echo " 0 = [BACK]"
         echo " 1 = Commands"
         echo " 2 = Services Files"
-        echo " 3 = [REALTIME] Services"
+        echo " 3 = RunLevels List"
+        echo " 4 = Services Running"
+        echo " 5 = Services Full List"
+        echo " 6 = [REALTIME] Services Running"
         echo "$ADM_DIVIDER"
 
         echo -n "Enter option: "
@@ -25,17 +28,37 @@ do
     else
 
         if [ "$option" == "1" ]; then
-            echo "- ls --color=auto -lAs /etc/init.d/*"
-            echo "- top"
+            echo "- ls --color=auto -lAs /etc/init.d/"
+            echo "- rc-status -l"
+            echo "- rc-status -a"
+            echo "- rc-status -s"
         fi
 
         if [ "$option" == "2" ]; then
-            list=$( ls --color=auto -lAs /etc/init.d/* )
+            list=$( ls --color=auto -lAs /etc/init.d/ )
             echo "$list"
         fi
 
         if [ "$option" == "3" ]; then
-            top
+            rc-status -l
+        fi
+
+        if [ "$option" == "4" ]; then
+            rc-status -a
+        fi
+
+        if [ "$option" == "5" ]; then
+            rc-status -s
+        fi
+
+        if [ "$option" == "6" ]; then
+            option=""
+            while [ "$option" == "" ]
+            do 
+                clear
+                rc-status -a
+                read -r -s -n 1 -t 0.25 option
+            done
             option="DONTWAIT"
         fi
 
