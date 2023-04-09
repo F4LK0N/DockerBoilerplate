0415ecf040ca29924a4185080079ce0b0d3207f6
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-09 04:01:28 -0300

    Adm - Services

--- .docker/alpine/os/adm/services.sh
+++ .docker/alpine/os/adm/services.sh
@@ -15,8 +15,8 @@ do
 
         echo " 0 = [BACK]"
         echo " 1 = Commands"
-        echo " 2 = Services Status"
-        echo " 3 = Services Files"
+        echo " 2 = Services Files"
+        echo " 3 = [REALTIME] Services"
         echo "$ADM_DIVIDER"
 
         echo -n "Enter option: "
@@ -25,23 +25,26 @@ do
     else
 
         if [ "$option" == "1" ]; then
-            echo "- service --status-all"
             echo "- ls --color=auto -lAs /etc/init.d/*"
+            echo "- top"
         fi
 
         if [ "$option" == "2" ]; then
-            list=$( service --status-all )
-            echo "$list"
-        fi
-
-        if [ "$option" == "3" ]; then
             list=$( ls --color=auto -lAs /etc/init.d/* )
             echo "$list"
         fi
 
-        echo "$ADM_DIVIDER"
-        echo -n "Enter to continue..."
-        read -r -s -n 1 option
+        if [ "$option" == "3" ]; then
+            top
+            option="DONTWAIT"
+        fi
+
+        
+        if [ "$option" != "DONTWAIT" ]; then
+            echo "$ADM_DIVIDER"
+            echo -n "Enter to continue..."
+            read -r -s -n 1 option
+        fi
         option="---"
 
     fi
