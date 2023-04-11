c01c5f29743b912150ad427a213d0c2acb3ba012
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-11 03:39:02 -0300

    Adm - System

--- .docker/phalcon/os/adm/system.sh
+++ .docker/phalcon/os/adm/system.sh
@@ -8,37 +8,33 @@ echo "### SYSTEM INFO ###"
 echo "$ADM_DIVIDER"
 
 
-distribution=$( cat /etc/os-release | grep "^NAME" | cut -d\" -f2 )
+distribution=$( cat /etc/os-release | grep "^NAME" | cut -d\" -f2 | cut -d' ' -f1 )
 version=$( cat /etc/os-release | grep VERSION_ID | cut -d\= -f2 )
-
-echo "OS Distribution : $distribution"
-echo "OS Version      : $version"
+echo "OS Distribution  : $distribution"
+echo "OS Version       : $version"
 echo "$ADM_DIVIDER"
 
 
 if [ -x "$(command -v httpd)" ]; then
-    httpd_version=$( httpd -v | grep version | cut -d\: -f2 | sed 's/ *$//g' )
-    httpd_version=`echo $httpd_version | sed 's/ *$//g'`
-
-    httpd_built=$( httpd -v | grep built | cut -d\: -f2 | sed 's/ *$//g' )
-    httpd_built=`echo $httpd_built | sed 's/ *$//g'`
-  
-    echo "HTTPD Version   : $httpd_version"
-    echo "HTTPD Built     : $httpd_built"
+    apache_version=$( httpd -v | grep "^Server version:" | awk -F 'Apache/' '{print $2}' | cut -d' ' -f1 )
+    apache_built=`echo $( httpd -v | grep "^Server built:" | awk -F 'built: ' '{print $2}' )`
+    
+    echo "Apache Version   : $apache_version"
+    echo "Apache Built     : $apache_built"
     echo "$ADM_DIVIDER"
 fi
 
 
 if [ -x "$(command -v php)" ]; then
     php_version=$( php -v | grep "^PHP" | cut -d' ' -f2 | sed 's/ *$//g' )
-    php_version=`echo $php_version | sed 's/ *$//g'`
-
-    php_built=$( php -v | grep built | cut -d"built:" -f2 | sed 's/ *$//g' )
-    php_built=$( php -v | grep built | awk -F 'built: ' '{print $2}' | awk -F ')' '{print $1}' )
-    #php_built=`echo $php_built | sed 's/ *$//g'`
-  
-    echo "PHP Version   : $php_version"
-    echo "PHP Built     : $php_built"
+    php_connection=$( php -v | grep "^PHP" | cut -d'(' -f2 | awk -F ')' '{print $1}' )
+    php_threads_mode=$( php -v | grep "^PHP" | cut -d'(' -f4 | awk -F ')' '{print $1}' )
+    php_built=$( php -v | grep "(built:" | awk -F 'built: ' '{print $2}' | awk -F ')' '{print $1}' )
+    
+    echo "PHP Version      : $php_version"
+    echo "PHP Connection   : $php_connection"
+    echo "PHP Thread Mode  : $php_threads_mode"
+    echo "PHP Built        : $php_built"
     echo "$ADM_DIVIDER"
 fi
 
