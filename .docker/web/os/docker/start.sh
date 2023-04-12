5314609ccc100a0f791460f849fe6f8e2789400a
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-12 06:51:25 -0300

    Container Start Script Improved

--- .docker/web/os/docker/start.sh
+++ .docker/web/os/docker/start.sh
@@ -2,49 +2,52 @@
 set -e
 
 
+echo "Directories - Logs"
+rm -rf \
+    /logs/os \
+    /logs/apache \
+    /logs/php;
+mkdir --mode=777 --parents \
+    /logs/os \
+    /logs/apache \
+    /logs/php;
+chmod 777 \
+    /logs \
+    /logs/os \
+    /logs/apache \
+    /logs/php;
+
+echo "Directories - Data"
+mkdir --mode=777 --parents \
+    /data/os \
+    /data/apache \
+    /data/php;
+chmod 777 \
+    /data \
+    /data/os \
+    /data/apache \
+    /data/php;
+
+
+
 echo "OS - Logs"
-rm -r -f /logs/os/
-mkdir -p /logs/os/
-chmod 777 /logs/os/
 ln -s /var/log/ /logs/os/
 
 
 
-echo "Apache - Logs"
-rm -r -f /logs/apache/
-mkdir -p /logs/apache/
-chmod 777 /logs/apache/
-
-
-
 echo "PHP - Data"
-mkdir -p /data/php/tmp/
-chmod 777 /data/php/tmp/
-
-echo "PHP - Logs"
-mkdir -p /logs/php/
-chmod 777 /logs/php/
-
-rm -f /logs/php/errors.log
-touch /logs/php/errors.log
-chmod 777 /logs/php/errors.log
-
-rm -f /logs/php/xdebug.log
-touch /logs/php/xdebug.log
-chmod 777 /logs/php/xdebug.log
-
-rm -r -f /logs/php/xdebug_tmp
-mkdir -p /logs/php/xdebug_tmp
-chmod 777 /logs/php/xdebug_tmp
-chmod 777 -R /logs/php/xdebug_tmp
+mkdir --mode=777 --parents /data/php/tmp
+chmod 777 /data/php/tmp
 
 
 
-echo "Application - File System"
-mkdir -p /app/
-mkdir -p /app/public/
-chmod 777 /app/
-#chmod 777 -R /app/
+echo "Application - Source"
+mkdir --mode=777 --parents \
+    /app \
+    /app/public;
+chmod 777 \
+    /app \
+    /app/public;
 
 echo "Application - Composer"
 if [[ ! -f "/app/composer.json" ]]; then
