6568799a0898b49e5d2697da129f7f0bd625b5ed
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-14 02:52:01 -0300

    Docker Start

--- .docker/web/os/docker/start.sh
+++ .docker/web/os/docker/start.sh
@@ -1,67 +1,34 @@
 #!/bin/sh
 set -e
 
-
-
-echo "FILESYSTEM - CLEAN";
-rm -rf ${OS_LOGS} \
-rm -rf ${OS_RUN} \
-rm -rf ${HTTPD_LOGS} \
-rm -rf ${HTTPD_RUN} \
-rm -rf ${PHP_LOGS} \
-rm -rf ${PHP_RUN};
-
 echo "FILESYSTEM - CREATE";
-# Container
-mkdir --mode=777 --parents /docker
-mkdir --mode=777 --parents /adm
-mkdir --mode=777 --parents ${ROOT_DATA}
-mkdir --mode=777 --parents ${ROOT_LOGS}
-mkdir --mode=777 --parents ${ROOT_RUN}
 # OS
-mkdir --mode=777 --parents ${OS_DATA}
-mkdir --mode=777 --parents ${OS_LOGS}
-mkdir --mode=777 --parents ${OS_RUN}
+mkdir -p --mode=777 ${OS_DATA}
+mkdir -p --mode=777 ${OS_LOGS}
 # Apache
-mkdir --mode=777 --parents ${HTTPD_DATA}
-mkdir --mode=777 --parents ${HTTPD_LOGS}
-mkdir --mode=777 --parents ${HTTPD_RUN}
-mkdir --mode=777 --parents ${HTTPD_RUN}/mutex
+mkdir -p --mode=777 ${HTTPD_DATA}
+mkdir -p --mode=777 ${HTTPD_LOGS}
 # PHP
-mkdir --mode=777 --parents ${PHP_DATA}
-mkdir --mode=777 --parents ${PHP_LOGS}
-mkdir --mode=777 --parents ${PHP_RUN}
+mkdir -p --mode=777 ${PHP_DATA}
+mkdir -p --mode=777 ${PHP_LOGS}
 # Application
-mkdir --mode=777 --parents ${APP_ROOT}
-mkdir --mode=777 --parents ${APP_ROOT}/public
+mkdir -p --mode=777 ${APP_ROOT}/public
 
 echo "FILESYSTEM - PERMISSIONS";
-# Container
-chmod 777 /docker
-chmod 777 /adm
-chmod 777 ${ROOT_DATA}
-chmod 777 ${ROOT_LOGS}
-chmod 777 ${ROOT_RUN}
 # OS
 chmod 777 ${OS_DATA}
 chmod 777 ${OS_LOGS}
-chmod 777 ${OS_RUN}
 # Apache
 chmod 777 ${HTTPD_DATA}
 chmod 777 ${HTTPD_LOGS}
-chmod 777 ${HTTPD_RUN}
-chmod 777 ${HTTPD_RUN}/mutex
 # PHP
 chmod 777 ${PHP_DATA}
 chmod 777 ${PHP_LOGS}
-chmod 777 ${PHP_RUN}
 # Application
 chmod 777 ${APP_ROOT}
 chmod 777 ${APP_ROOT}/public
 
-
-
-echo "OS - LOGS";
+echo "LOGS";
 ln -s /var/log/ ${OS_LOGS}
 
 echo "APPLICATION - COMPOSER"
