1bc60e42b47985c165cd373291f0c1d2e522c4ee
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-16 20:45:14 -0300

    Core - Header

--- src/core/headers.php
+++ src/core/headers.php
@@ -7,12 +7,112 @@ class eHEADER_CONTENT_TYPE
 }
 class HEADERS
 {
+    static private $CORS_REQUEST      = false;
+    static private $PREFLIGHT_REQUEST = false;
+
+
+
     static public function LOAD()
 	{
-		self::CONTENT_TYPE();
-		self::CORS();
-		self::OPTIONS();
+        self::IDENTIFY_REQUEST();
+
+		self::HANDLE_CORS_REQUEST();
+        self::HANDLE_PREFLIGHT_REQUEST();
+        self::HANDLE_CONTENT_REQUEST();
 	}
+
+    static private function IDENTIFY_REQUEST()
+    {
+        self::$CORS_REQUEST = isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN']!=='';
+        
+        self::$PREFLIGHT_REQUEST = (
+            $_SERVER['REQUEST_METHOD']==='OPTIONS' ||
+            ($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHODS']??false) ||
+            ($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']??false)
+        );
+    }
+
+    static private function HANDLE_CORS_REQUEST () 
+    {
+        if(!self::$CORS_REQUEST){
+            return;
+        }
+
+
+        //Allow Origin
+        $origin = '*';
+        if(self::$PREFLIGHT_REQUEST){
+            $origin = $_SERVER['HTTP_ORIGIN'];
+        }
+        header("Access-Control-Allow-Origin: $origin");
+
+
+        //Allow Methods
+        $allowMethods   = ['GET', 'POST', 'OPTIONS'];
+        $requestMethods = explode(',', $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHODS'] ?? '');
+        foreach($requestMethods as $key){
+            $key = trim($key);
+            if($key){
+                $allowMethods[] = strtoupper($key);
+            }
+        }
+        $allowMethods = array_unique($allowMethods);
+        $allowMethods = implode(', ', $allowMethods);
+        header("Access-Control-Allow-Methods: $allowMethods");
+
+        
+        //Allow Headers
+        $allowHeaders   = ['content-type', 'accept-encoding', 'x-requested-with'];
+        $requestHeaders = explode(',', $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'] ?? '');
+        foreach($requestHeaders as $key){
+            $key = trim($key);
+            if($key){
+                $allowHeaders[] = strtolower($key);
+            }
+        }
+        $allowHeaders = array_unique($allowHeaders);
+        $allowHeaders = implode(', ', $allowHeaders);
+        header("Access-Control-Allow-Headers: $allowHeaders");
+        
+
+        //Max Age
+        if(!self::$PREFLIGHT_REQUEST){
+            header('Access-Control-Max-Age: 0');
+        }
+
+    }
+
+    static private function HANDLE_PREFLIGHT_REQUEST () 
+    {
+        if(!self::$PREFLIGHT_REQUEST){
+            return;
+        }
+
+        self::CONTENT_TYPE(eHEADER_CONTENT_TYPE::HTML);
+        self::CACHE_CONTROL(true);
+        self::EXPIRES();
+        self::E_TAG('CORS');
+
+        //Max Age
+        header('Access-Control-Max-Age: 86400');
+        
+        //Credentials
+		header('Access-Control-Allow-Credentials: true');
+
+        //Exit
+        http_response_code(204);
+        exit(0);
+    }
+
+    static private function HANDLE_CONTENT_REQUEST () 
+    {
+        self::CONTENT_TYPE(eHEADER_CONTENT_TYPE::JSON);
+		self::CACHE_CONTROL(false);
+		self::EXPIRES();
+		self::E_TAG();
+    }
+
+
 
 	static public function CONTENT_TYPE (int $type=null)
 	{
@@ -21,28 +121,37 @@ class HEADERS
 		//Type
 		$header .= ($type===eHEADER_CONTENT_TYPE::HTML)?"text/html;":"application/json;";
 		//Encoding
-		$header.=" charset=utf-8";
+		$header.=" charset=UTF-8";
 		
 		header($header);
 	}
-	
-	static public function CORS()
+
+    static public function CACHE_CONTROL ($useCache=false)
 	{
-		header('Access-Control-Allow-Origin: *');
-		header('Access-Control-Allow-Credentials: true');
-		header('Access-Control-Max-Age: 86400');
-		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-		header("Access-Control-Allow-Headers: " . (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']) ? $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'].", " : "")."X-Requested-With, Accept-Encoding");
-	}
-	static public function OPTIONS()
-	{
-		//OPTIONS (Pre-flight requests)
-		//If the $_SERVER['REQUEST_METHOD'] is of the type "OPTIONS" is probably because the ajax plugin from the front-end is performing a 'pre-flight' request.
-		//A 'pre-flight' is a request to the API to know the accepted content formats, compression, cross-origins requests and etc.
-		//In this case the server only need to respond with http headers and don't need to enter inside the API logic, saving resources.
-		if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
-			exit(0);
+        if($useCache){
+            header('Cache-Control: no-cache, no-store, max-age=0, must-revalidate');
+            header('pragma: no-cache');
+        }else{
+
         }
+	}
+
+    static public function EXPIRES ($lifetime=0)
+	{
+        if($lifetime===0){
+		    header('Age: 0');
+            header('Expires: -1');
+        }else{
+
+        }
+	}
+
+    static public function E_TAG ($tag=null)
+	{
+        if($tag===null){
+            $tag=microtime();
+        }
+		header('ETag: '.sha1($tag));
 	}
 
 }
