affd0d5b306f8c122a1a4b46983ca217441e1ed4
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-16 10:31:43 -0300

    App Core

--- src/app/_application/_application.php
+++ src/app/_application/_application.php
@@ -1,11 +1,17 @@
 <?php
 use Phalcon\Mvc\Application;
+use Phalcon\Mvc\Router;
+use Phalcon\Mvc\Dispatcher;
+use Phalcon\Mvc\View;
+use Phalcon\Http\Response;
 
 class _APPLICATION
 {
-    static private $INSTANCE = null; 
+    static private ?Application $INSTANCE = null;
 
-    static public function INSTANCE()
+
+
+    static public function INSTANCE(): Application
     {
         if(self::$INSTANCE === null){
             self::$INSTANCE = new Application(PROVIDER::INSTANCE());
@@ -13,36 +19,66 @@ class _APPLICATION
         return self::$INSTANCE;
     }
 
+    static public function LOAD()
+    {
+        self::INSTANCE();
+    }
+
     static public function RUN()
     {
         try {
-            $router = PROVIDER::GET("router");
-
-            $view = PROVIDER::GET('view');
-
-            $dispatcher = PROVIDER::GET('dispatcher');
-            $dispatcher->setControllerName($router->getControllerName());
-            $dispatcher->setActionName($router->getActionName());
-            $dispatcher->setParams($router->getParams());
-
-            $view->start();
-            $dispatcher->dispatch();
-            $view->render(
-                $dispatcher->getControllerName(),
-                $dispatcher->getActionName(),
-                $dispatcher->getParams()
-            );
-            $view->finish();
-
-            $response = PROVIDER::GET('response');
-            $response->setContent($view->getContent());
-            $response->send();
+            $dispatcher = self::RUN_DISPATCHER();
+            $view       = self::RUN_VIEW($dispatcher);
+            self::RUN_RESPONSE($view);
         }
         catch (\Exception $e) {
             echo 'Exception: ', $e->getMessage();
         }
     }
 
+    static private function RUN_DISPATCHER(): Dispatcher
+    {
+        /**
+         * @var Router
+         */
+        $router = PROVIDER::GET("router");
+        /**
+         * @var Dispatcher
+         */
+        $dispatcher = PROVIDER::GET('dispatcher');
+        $dispatcher->setControllerName($router->getControllerName());
+        $dispatcher->setActionName($router->getActionName());
+        $dispatcher->setParams($router->getParams());
+        return $dispatcher;
+    }
+
+    static private function RUN_VIEW(Dispatcher &$dispatcher): View
+    {
+        /**
+         * @var View
+         */
+        $view = PROVIDER::GET('view');
+        $view->start();
+        $dispatcher->dispatch();
+        $view->render(
+            $dispatcher->getControllerName(),
+            $dispatcher->getActionName(),
+            $dispatcher->getParams()
+        );
+        $view->finish();
+        return $view;
+    }
+
+    static private function RUN_RESPONSE(View &$view)
+    {
+        /**
+         * @var Response
+         */
+        $response = PROVIDER::GET('response');
+        $response->setContent($view->getContent());
+        $response->send();
+    }
+
 }
 
-_APPLICATION::RUN();
+_APPLICATION::LOAD();
