343636bf1594c38dda0376937915d263c7a6582a
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-16 23:36:19 -0300

    Users Table

--- src/app/_config/db/users.sql
+++ src/app/_config/db/users.sql
@@ -1,8 +1,37 @@
 CREATE TABLE `users` (
-    `id`        bigint NOT NULL AUTO_INCREMENT,
-    `email`     text NULL,
-    `password`  text NULL,
-    `name`      text NULL,
+    `id`
+        bigint
+        NOT NULL
+        AUTO_INCREMENT,
+    
+    `access_type`
+        ENUM(
+            'dev',
+            'master',
+            'admin',
+            'manager',
+            'editor',
+            'reader'
+        ) 
+        NOT NULL
+        DEFAULT 'reader',
+    
+    `email`
+        VARCHAR(255)
+        NOT NULL,
+    
+    `pass`
+        TEXT
+        NOT NULL,
+    
+    `name`
+        VARCHAR(255)
+        NOT NULL,
+
+    `surname`
+        VARCHAR(255)
+        NOT NULL
+        DEFAULT '',
 
     PRIMARY KEY (`id`)
 )
