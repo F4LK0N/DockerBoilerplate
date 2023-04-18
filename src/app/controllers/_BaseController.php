67f2b5608d567a76b3bc80a78af7c2ba0717157b
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-18 10:04:25 -0300

    Controller Base Use ErrorCodes

--- src/app/controllers/_BaseController.php
+++ src/app/controllers/_BaseController.php
@@ -3,6 +3,32 @@ use Phalcon\Mvc\Controller;
 use Phalcon\Mvc\Dispatcher;
 use Phalcon\Http\Response;
 
+enum eRESPONSE_STATUS_CODES: int
+{
+    case SUCCESS = 1;
+    case ERROR   = 0;
+}
+
+enum eERROR_CODES: int
+{
+    case NO_ERROR    = 0;
+
+    //CONTROLLERS
+    case CONTROLLER  = 10000; //Generic Controller Error.
+    case CONTROLLER_INPUT       = 11000; //Error on the input (get, filter or validate)
+    case CONTROLLER_NOT_FOUND   = 12000; //When required, the register was not found on db.
+    case CONTROLLER_RULE        = 13000; //When set, some model rule was not complied.
+    case CONTROLLER_TRANSACTION = 14000; //When set, the db transaction has an error (querying, saving, updating, deleting)
+    case CONTROLLER_ENCODE      = 15000; //Error encoding the response in JSON format.
+
+    //INPUT
+    case INPUT            = 100; //Generic Input Error.
+    case INPUT_CONFIG     = 101; //Invalid Config
+    case INPUT_RETRIEVE   = 102; //Cannot Retrieve
+    case INPUT_FILTER     = 103; //Filter Error
+    case INPUT_VALIDATION = 104; //Validation Error
+}
+
 class _BaseController extends Controller
 {
     protected $apiResponse = [];
@@ -10,9 +36,9 @@ class _BaseController extends Controller
     public function beforeExecuteRoute(Dispatcher $dispatcher): bool
     {
         $this->apiResponse = [
-            'status' => 1,
+            'status' => eRESPONSE_STATUS_CODES::SUCCESS,
             'error' => [
-                'code'    => 0,
+                'code'    => eERROR_CODES::NO_ERROR,
                 'message' => '',
                 'details' => '',
             ],
@@ -26,12 +52,12 @@ class _BaseController extends Controller
         $this->apiResponse['data'] = $data;
     }
 
-    protected function setError(string $message, int $code=0, $details='')
+    protected function setError(string $message, int $code=null, $details='')
     {
         $this->apiResponse = [
-            'status' => 0,
+            'status' => eRESPONSE_STATUS_CODES::ERROR,
             'error' => [
-                'code'    => 400 + $code,
+                'code'    => ($code??eERROR_CODES::CONTROLLER),
                 'message' => $message,
                 'details' => $details,
             ],
@@ -40,7 +66,7 @@ class _BaseController extends Controller
         $this->send();
     }
 
-    public function afterExecuteRoute($dispatcher)
+    public function afterExecuteRoute(Dispatcher $dispatcher)
     {
         $this->send();
     }
