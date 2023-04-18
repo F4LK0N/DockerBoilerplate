1f13a3c4f98c8031140881bbe02c5fba5203164e
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-18 17:22:24 -0300

    Security Provider

---
+++ src/app/_provider/security.php
@@ -0,0 +1,37 @@
+<? defined("FKN") or http_response_code(403).die('Forbidden!');
+
+class Security
+{
+    public function passHash(string $email, string $pass): string
+    {
+        $iterations = (10 + (strlen($email)%3));
+        
+        for($i=0; $i<$iterations; $i++){
+            $email = md5(sha1($email));
+            $pass  = sha1(md5($pass));
+        }
+        for($i=0; $i<$iterations; $i++){
+            $email = md5(sha1($email.$pass));
+            $pass  = sha1(md5($email.$pass));
+        }
+        for($i=0; $i<$iterations; $i++){
+            $pass = substr(sha1(md5($pass)).md5(sha1($pass)).sha1($pass).md5($pass).sha1($pass), 32, 128);
+        }
+
+        return $pass;
+    }
+
+    public function sessionToken(): string
+    {
+        return substr($this->passHash(time(), microtime()), 32, 64);
+    }
+
+}
+
+PROVIDER::SET_SHARED(
+    'security',
+    function () {
+        $security = new Security();
+        return $security;
+    }
+);
