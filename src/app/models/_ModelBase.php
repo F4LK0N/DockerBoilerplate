d115b3ee6057858d129b71d3c8c628ca3bd1dd25
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-18 19:19:14 -0300

    Model Events Manager

--- src/app/models/_ModelBase.php
+++ src/app/models/_ModelBase.php
@@ -3,6 +3,60 @@
 use Phalcon\Mvc\Model;
 use Phalcon\Mvc\Model\Resultset;
 
+trait _Behavior_Status
+{
+    public function beforeCreate()
+    {
+        echo('STATUS beforeCreate');
+        echo(@$this->id).'\n';
+        $this->skipOperation(true);
+        
+    }
+    public function create(): bool
+    {
+        echo('STATUS create');
+        echo(@$this->id).'\n';
+        $this->skipOperation(true);
+        return true;
+    }
+    public function afterCreate()
+    {
+        echo('STATUS afterCreate');
+        echo(@$this->id).'\n';
+        $this->skipOperation(true);
+    }
+
+
+    public function beforeUpdate()
+    {
+        $this->skipOperation(true);
+        
+        //VD('STATUS beforeUpdate');
+    }
+    
+    public function beforeDelete()
+    {
+        $this->skipOperation(true);
+        
+        //VD('STATUS beforeDelete');
+    }
+    
+}
+
+trait _Behavior_Subversion
+{
+    public function beforeCreate()
+    {
+        
+    }
+
+    public function beforeUpdate()
+    {
+        
+    }
+    
+}
+
 class _ModelBase extends Model
 {
     public function getMessagesString($messages='')
