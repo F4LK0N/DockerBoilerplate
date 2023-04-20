cc922a807ee930b3d654fbbc8fb3102370037198
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-20 20:02:25 -0300

    Helper Time

--- src/core/helpers/time.php
+++ src/core/helpers/time.php
@@ -28,43 +28,11 @@ class TIME
 
     static private function FORMAT_MICROSECONDS (&$time)
     {
-        //Seconds
-//        $s = explode(".", $time, 2)[0];
         $s = floor($time);
-        
-        //MilliSeconds
         $ms = intval(($time * 1000)%1000);
-        
         return 
             str_pad($s, 3, "_", STR_PAD_LEFT)."s ".
             str_pad($ms, 3, "_", STR_PAD_LEFT)."ms";
     }
     
 }
-
-
-$sStart = TIME::SECONDS();
-$mStart  = TIME::MICROSECONDS();
-
-$data=[];
-for($i=0; $i<10000; $i++)
-{
-    for($i2=0; $i2<3000; $i2++)
-        $data[$i] = $i * $i2;
-    
-}
-
-$sEnd = TIME::SECONDS($sStart);
-$mEnd = TIME::MICROSECONDS($mStart);
-
-
-
-
-
-
-VD($sStart);
-VD($sEnd);
-VD(TIME::FORMAT($sEnd));
-VD($mStart);
-VD($mEnd);
-VDD(TIME::FORMAT($mEnd));
