fb53d66dd5537a7a319cde1d9b1692df6bc33df3
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-20 19:52:51 -0300

    Helper Time

--- src/core/helpers/time.php
+++ src/core/helpers/time.php
@@ -23,64 +23,48 @@ class TIME
 
     static private function FORMAT_SECONDS (&$time)
     {
-        return str_pad($time, 6, "_", STR_PAD_LEFT)."s";
+        return str_pad($time, 3, "_", STR_PAD_LEFT)."s";
     }
 
     static private function FORMAT_MICROSECONDS (&$time)
     {
-        $time = round((float)$time, 6, PHP_ROUND_HALF_UP);
-
-        VD($time);
-        
         //Seconds
-        $time = explode(".", $time, 2);
-        $s = $time[0];
+//        $s = explode(".", $time, 2)[0];
+        $s = floor($time);
         
         //MilliSeconds
-        $time = (float)@$time[1];
-        $time = explode(".", $time, 2);
-        $ms = $time[0];
+        $ms = intval(($time * 1000)%1000);
         
-        //MicroSeconds
-        $time = (float)@$time[1];
-        $time = explode(".", $time, 2);
-        $us = $time[0];
-        
-        
-        
-        $s = str_pad($s, 6, "_", STR_PAD_LEFT)."s";
-        $ms = str_pad($ms, 4, "_", STR_PAD_LEFT)."ms";
-        $us = str_pad($us, 4, "_", STR_PAD_LEFT)."us";
-        
-        return $s.$ms.$us;
+        return 
+            str_pad($s, 3, "_", STR_PAD_LEFT)."s ".
+            str_pad($ms, 3, "_", STR_PAD_LEFT)."ms";
     }
     
 }
 
-$timeSecond = TIME::SECONDS();
-VD($timeSecond);
-$data=[];
-for($i=0; $i<10000; $i++)
-{
-    for($i2=0; $i2<1000; $i2++)
-    {
-        $data[$i] = $i * $i2;
-    }
-}
-$timeSecond = TIME::SECONDS($timeSecond);
-VD($timeSecond);
-VD(TIME::FORMAT($timeSecond));
 
-$timeMicro  = TIME::MICROSECONDS();
-VD($timeMicro);
+$sStart = TIME::SECONDS();
+$mStart  = TIME::MICROSECONDS();
+
 $data=[];
 for($i=0; $i<10000; $i++)
 {
-    for($i2=0; $i2<1000; $i2++)
-    {
+    for($i2=0; $i2<3000; $i2++)
         $data[$i] = $i * $i2;
-    }
+    
 }
-$timeMicro = TIME::MICROSECONDS($timeMicro);
-VD($timeMicro);
-VDD(TIME::FORMAT($timeMicro));
+
+$sEnd = TIME::SECONDS($sStart);
+$mEnd = TIME::MICROSECONDS($mStart);
+
+
+
+
+
+
+VD($sStart);
+VD($sEnd);
+VD(TIME::FORMAT($sEnd));
+VD($mStart);
+VD($mEnd);
+VDD(TIME::FORMAT($mEnd));
