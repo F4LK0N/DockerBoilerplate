0c86eb4ea445791fb5058f6990daa77cbbb4ef5c
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-23 02:26:05 -0300

    Entrypoint Scripts

--- .docker/api/os/docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "### CONTAINER START ###"
-/docker/start.sh
-
-echo "### CONTAINER RUN ###"
-exec /docker/run.sh "$@"
--- .docker/api/os/docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Apache - Server Daemon"
-exec httpd -D FOREGROUND
