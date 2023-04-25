794a8cfec8d2aac05eccb1dd9c3cd48056efbc26
Otávio B. Sória <F4LK0N@users.noreply.github.com>
2023-04-25 04:34:10 -0300

    Entrypoint

--- .docker/debian/os/docker/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-trap "echo '### CONTAINER STOP ###'; exit" SIGWINCH
-
-while true; do
-    sleep 1
-done
--- .docker/debian/os/docker/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "OS - Logs Folder"
-rm -r -f /logs/debian/
-mkdir -p /logs/debian/
-ln -s /var/log/ /logs/debian/
